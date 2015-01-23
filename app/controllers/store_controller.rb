482147583506f5ab4cf77d16720df63334509c3f
Max Trubetskoy <handcrab@gmail.com>
2015-01-23 15:10:00 +0000

    create cart and line item scaffolds

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,12 @@
 class StoreController < ApplicationController
   def index
     @products = Product.order :title
+
+    increment_counter    
+  end
+
+  def increment_counter
+    session[:counter] ||= 1 # if session[:counter].nil?
+    session[:counter] += 1
   end
 end
--- app/helpers/store_helper.rb
+++ app/helpers/store_helper.rb
@@ -1,2 +1,7 @@
 module StoreHelper
+
+  def visit_counter_message
+    counter = session[:counter]
+    "You've visited this page #{ counter } #{ 'time'.pluralize counter }"
+  end
 end
