0b6008f1e995b29acedc17584c5b92b6719b7450
Max Trubetskoy <handcrab@gmail.com>
2015-02-06 20:24:51 +0000

    create session scaffold for auth

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,23 @@
 Rails.application.routes.draw do
+
+  get 'admin' => 'admin#index'
+  controller :sessions do
+    get 'login' => :new
+    post 'login' => :create
+    delete 'logout' => :destroy
+  end
+  # get 'sessions/new'
+  # get 'sessions/create'
+  # get 'sessions/destroy'
+
   resources :users
-
   resources :orders
-
   resources :line_items
-
   resources :carts
 
   get 'store/index'
 
-  resources :products
+  # resources :products
 
   # The priority is based upon order of creation:
   # (Приоритет основан на порядке создания:)
